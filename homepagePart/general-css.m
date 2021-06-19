c2d68774ebb8b890e7b740e27f274886c815eb91
liew312 <56454775+liew312@users.noreply.github.com>
2021-06-19 22:10:24 +0800

    Create general-css.m

---
+++ homepagePart/general-css.m
@@ -0,0 +1,46 @@
+\
+\ Copyright © 2021 liew0183 all rights reserved.
+\
+\ @author: liew0183
+\ @date: 18 Jun 2021
+\
+\ A brief description of this program.
+\ general css
+
+: yesButton ( "s" -- "s" )
+  q{ 
+	margin-left:10px;
+	margin-top:10px;
+	padding:10px;
+	font-size:15px;
+	color:#FFFFFF;
+	background:#4CAF50;
+  }q  (button)
+;
+
+
+: noButton ( "s" -- "s" )
+  q{ 
+	margin-left:10px;
+	margin-top:10px;
+	padding:10px;
+	font-size:15px;
+	color:#FFFFFF;
+	background:#f44336;
+  }q  (button)
+;
+
+: generalButton ( "s" -- "s" )
+  q{ 
+	margin-left:10px;
+	margin-top:10px;
+	padding:10px;
+	text-align: center;
+	width: 160px;
+	font-size:15px;
+	color:#FFFFFF;
+	background:#53284e;
+  }q  (button)
+;
+
+
