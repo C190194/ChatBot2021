f318f3c84ee88f68b5b36f1299dab91ba1e58cbe
liew312 <56454775+liew312@users.noreply.github.com>
2021-07-10 14:28:45 +0800

    Update track_record.m

--- topics/About_Us/track_record.m
+++ topics/About_Us/track_record.m
@@ -7,6 +7,11 @@
 \ A brief description of this program.
 \
 
+mem: trackRecordPageNum
+    1 trackRecordPageNum!
+
+assoc: track-record-page-content
+
 : mk-track-record ( "s" "title" -- "s" ) { title imgAddress description pax }
     title ctx{ title}
     imgAddress ctx{ imgAddress}
@@ -26,10 +31,7 @@
 : track-record ( "s" -- "s" )
 "Largest Buffet Catering" "https://sethlui.com/wp-content/uploads/2017/04/Savory-Kitchen-ONLINE-1.jpg?ezimgfmt=ng:webp/ngcb5" "SISPEC GRADUATION AUG 2019" "3,500 Pax" mk-track-record ctx{ track1 }
 "Largest BBQ Live-Cooking" "https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F1177886278%2Fx.jpg" "People’s Association HQ" "800 Pax" mk-track-record ctx{ track2 }
-"Largest Last Minute Order" "https://l.hdnux.com/350x235p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/d3cee4585b3446de94b72244afa12a8d.jpg" "Sungei Gedong Camp" "800 Pax (3hour)" mk-track-record ctx{ track3 }
-"Largest Packed Meal" "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153057.898.jpeg" "Changi Prison Cluster A" "6,300 Pax" mk-track-record ctx{ track4 }
-"Largest Continuous Order" "https://scontent.fsin9-2.fna.fbcdn.net/v/t1.6435-9/123446589_4961250193915447_5288204296137057160_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=a26aad&_nc_ohc=qAbzACVQNMcAX_TTV4m&_nc_ht=scontent.fsin9-2.fna&oh=b80fd827a67de51da8505015a8cf5489&oe=60EA3368" "Sports SG (Get Active SG 2019)" "15,000 Pax (1 mth)" mk-track-record ctx{ track5 }
-"Largest Order/Day" "https://lerainbow.com.sg/wp-content/uploads/2020/10/DSC_3365-2-300x200.jpg" "Aug, Year 2019" "78 Orders" mk-track-record ctx{ track6 }
+"Next Page" backButton ctx{ nextPageBtn }
 "Back" backButton ctx{ backBtn }
   q{
     <h4 style="text-align:center;font-size:25px">Track record</h4>
@@ -38,16 +40,73 @@
     <br>
     #{track2}
     <br>
-    #{track3}
-    <br>
-    #{track4}
-    <br>
-    #{track5}
-    <br>
-    #{track6}
+    #{nextPageBtn}
     <br>
     #{backBtn}
   }q
   
 ;
+
+: track-record-page2 ( "s" -- "s" )
+"Largest Last Minute Order" "https://l.hdnux.com/350x235p/s3-us-west-1.amazonaws.com/contentlab.studiod/getty/d3cee4585b3446de94b72244afa12a8d.jpg" "Sungei Gedong Camp" "800 Pax (3hour)" mk-track-record ctx{ track3 }
+"Largest Packed Meal" "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153057.898.jpeg" "Changi Prison Cluster A" "6,300 Pax" mk-track-record ctx{ track4 }
+"Previous Page" backButton ctx{ previousPageBtn }
+"Next Page" backButton ctx{ nextPageBtn }
+"Back" backButton ctx{ backBtn }
+  q{
+    <h4 style="text-align:center;font-size:25px">Track record</h4>
+    <br>
+    #{track3}
+    <br>
+    #{track4}
+    <br>
+    #{previousPageBtn} #{nextPageBtn}
+    <br>
+    #{backBtn}
+  }q
+  
+;
+
+
+: track-record-page3 ( "s" -- "s" )
+"Largest Continuous Order" "https://scontent.fsin9-2.fna.fbcdn.net/v/t1.6435-9/123446589_4961250193915447_5288204296137057160_n.jpg?_nc_cat=100&ccb=1-3&_nc_sid=a26aad&_nc_ohc=qAbzACVQNMcAX_TTV4m&_nc_ht=scontent.fsin9-2.fna&oh=b80fd827a67de51da8505015a8cf5489&oe=60EA3368" "Sports SG (Get Active SG 2019)" "15,000 Pax (1 mth)" mk-track-record ctx{ track5 }
+"Largest Order/Day" "https://lerainbow.com.sg/wp-content/uploads/2020/10/DSC_3365-2-300x200.jpg" "Aug, Year 2019" "78 Orders" mk-track-record ctx{ track6 }
+"Previous Page" backButton ctx{ previousPageBtn }
+"Back" backButton ctx{ backBtn }
+q{
+    <h4 style="text-align:center;font-size:25px">Track record</h4>
+    <br>
+    #{track5}
+    <br>
+    #{track6}
+    <br>
+    #{previousPageBtn}
+    <br>
+    #{backBtn}
+  }q
+  
+;
+
+{{
+    1 track-record
+    2 track-record-page2
+    3 track-record-page3
+    
+}} +track-record-page-content
+
+room: track-record-room
+Q: Why Le Rainbow
+Q: Back
+A: ${ why-lerainbow }
+K: -track-record-room
+--
+
+Q: Next Page
+A: +trackRecordPageNum % ${trackRecordPageNum track-record-page-content}
+--
+
+Q: Previous Page
+A: trackRecordPageNum 1 - trackRecordPageNum! % ${trackRecordPageNum track-record-page-content}
+--
+end-room
 
