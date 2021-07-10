95eb24b5a6daef7ad2ca2951c64e30160af7539a
C190194 <73808832+C190194@users.noreply.github.com>
2021-07-10 14:55:45 +0800

    Update faq_inquiries.m

--- topics/faq/faq_inquiries.m
+++ topics/faq/faq_inquiries.m
@@ -11,21 +11,39 @@
 \ Remember to log the qn when they send it
 
 
-Q: Back to all services
-A: ${ faq-welcome }
-K: $back services-faq
+Q: Back to our services
+A: ${ faq-explore }
+K: $clear services-faq
 --
 
-: faq-inquiry-thanks
-"Back to all services" backButton ctx{ backBtn }
+: faq-inquiry-thanks_1
+"Back to Our Services" backButton ctx{ backBtn }
 q{
-    <h3>Thanks! We will get back to you 👍🏼</h3>
+    <h3>Thanks! We will get back to you.</h3>
     </br>
     #{backBtn}
 }q
 ;
+
+: faq-inquiry-thanks_2
+"Yes" yesButton ctx{ yes }
+"No" noButton ctx{ no }
+q{
+    <h3>Thanks! We have received your message.</h3>
+    </br>
+    <p>Would you like to leave your contact info to get our reply?</p>
+    #{yes} #{no}
+}q
+;
+
+
+
 Q: $_
-A: ${ faq-inquiry-thanks }
-L: [Customer inquiry] userName:${ userName } | ${ last-question } 
+A: name #empty? not % ${ faq-inquiry-thanks_1 }
+L: name #empty? not % [Customer inquiry] userName: ${ name #keys head } | ${ last-question } 
+A: last-question inquiry_log! % ${ faq-inquiry-thanks_2 }
+K: name #empty? % waiting-for-permission
+C: name #empty? % 1 unlogged_inquiry!
 --
+
 
