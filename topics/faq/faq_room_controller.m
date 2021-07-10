8cb38a44fb9d6b8223642090295fb4e2e754612c
C190194 <73808832+C190194@users.noreply.github.com>
2021-07-10 14:53:50 +0800

    Update faq_room_controller.m

--- topics/faq/faq_room_controller.m
+++ topics/faq/faq_room_controller.m
@@ -6,36 +6,18 @@
 \
 \ A brief description of this program.
 \
- 
 
-: faq-welcome 
-  "Explore" faqMenuButton ctx{ exploreBtn }
-  "Back to Home" backButton ctx{ homeBtn }
-q{
-    <h2 style="text-align:center">Just ask!</h2>
-    <p>Simply ask any question you have about our food, our services, or our ordering process! </p>
-    </br>
-    
-    <h2 style="text-align:center">Or, explore.</h2>
-    <p>You can also choose to explore the questions by topic as well </br>
-    #{exploreBtn}
-    </p>
-    
-    </br>
-    #{homeBtn}
-}q
- ;
 
 : faq-explore
   "What events are you able to support?" button ctx{ generalBtn }
-  "Menu options and customisation" button ctx{ menuBtn }
+  "Food options and customisation" button ctx{ menuBtn }
   "Our value-added services" button ctx{ addOnBtn }
   "What is the order and delivery process?" button ctx{ orderDeliveryBtn }
   "How do I pay?" button ctx{ paymentBtn }
   "Send us a message" faqMenuButton ctx{ inquiryBtn }
   "Back to Home" backButton ctx{ homeBtn }
 q{
-    <h2 style="text-align:center">Exploring Services</h2>
+    <h2 style="text-align:center">Our Services</h2>
     <p>At Le Rainbow Catering, we offer a hassle-free catering experience. Click any topic to learn more!
     </br>
     #{generalBtn} #{menuBtn} #{addOnBtn} #{orderDeliveryBtn} #{paymentBtn}
@@ -55,71 +37,75 @@ q{
 
 \ Our inquiries message
 : faq-inquiries-msg
-  "Back to our services" backButton ctx{ backBtn }
+  "Back to Our Services" backButton ctx{ backBtn }
   "Contact Us" faqMenuButton ctx{ contactInfoBtn }
 q{
     <h2>How can we help?</h2>
     <p>
         Simply type your enquiry below. Our team will get in touch with you.
     </p>
-    </br>
+    <br>
     
     <h3>Prefer to talk to us?</h3>
     #{contactInfoBtn}
         
-    </br>
+    <br><br>
     
     <h3>Didn't mean to send a message?</h3>
+    <br>
     #{backBtn}
 
 }q
  ;
 
 
-: faq-contact-info
-  "Back to all services" backButton ctx{ backBtn }
-\   "msg" "sales@lerainbow.com.sg" "I need help" email ctx{ salesEmail }
-\   "+65 6589 8855" "+65 6589 8855" tel ctx{ salesTel }
-
-q{
-    <p>
-        You can contact us via email,<a href = "mailto: sales@lerainbow.com.sg">sales@lerainbow.com.sg</a>or telephone,<a href="tel:6589 8855">6589 8855</a>). We will be glad to help any way we can!
-        When you are done, click the button to explore our other services, or to go back to the main page. 
-        </br>
-        #{backBtn}
-    </p>
-}q
-;
+\ : faq-contact
+\   "Back to Home" backButton ctx{ backBtn }
+\   "Email" "sales@lerainbow.com.sg" "Question from Chatbot" email ", " concat
+\   "Telephone" "+65 658 98855" tel concat " or " concat
+\   "Our Website" "https://lerainbow.com.sg/contact/" link concat ctx{ links }
+\ q{
+\     <p>
+\         You can contact us via
+\     </p>
+\     #{links}
+\     <br><br>
+\     We will be glad to help any way we can!
+\     <br><br>
+\     #{backBtn}
+\ 
+\ }q
+\ ;
 
 
 Q: Send a message
+Q: Send us a message
 Q: Inquiries
 A: ${ faq-inquiries-msg }
 K: -services_faq services_inquiries
 --
 
-Q: Back to Home
-A: ${ home_menu }
-K: -services_faq -services_inquiries
---
+\ Q: Back to Home
+\ A: ${ homepage }
+\ K: -services_faq -services_inquiries
+\ --
 
 \ Go back from inquiries page
 Q: Back to our services
-A: ${ faq-welcome }
-K: $back services_faq
---
-
-\ Go back from any of the faq topics
-Q: Back
 A: ${ faq-explore }
+K: $clear services_faq
 --
 
-Q: Explore
-A: ${ faq-explore }s
+
+
+Q: Our services
+A: ${ faq-explore }
+K: services_faq
 --
 
 Q: Contact Us
-A: ${ faq-contact-info }
+A: ${ faq-contact }
+K: -services_inquiries
 --
 
 
