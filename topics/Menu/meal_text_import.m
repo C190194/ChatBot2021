a49c16350f18e09d7750868be8fbe81020142026
C190194 <73808832+C190194@users.noreply.github.com>
2021-07-19 20:20:31 +0800

    Create meal_text_import.m

---
+++ topics/Menu/meal_text_import.m
@@ -0,0 +1,45 @@
+\
+\ Copyright © 2021 huangrt all rights reserved.
+\
+\ @author: huangrt
+\ @date: 4 Jul 2021
+\
+\ A brief description of this program.
+\
+
+
+include ./topics/Menu/bento_meal/Exquisite_Bento/Boite_text.m
+include ./topics/Menu/bento_meal/Exquisite_Bento/Grande_text.m
+include ./topics/Menu/bento_meal/Exquisite_Bento/Gusto_text.m
+include ./topics/Menu/bento_meal/Exquisite_Bento/Mieux_text.m
+
+include ./topics/Menu/bento_meal/Rainbow_Bento/Bitez_text.m
+include ./topics/Menu/bento_meal/Rainbow_Bento/Delicieux_text.m
+include ./topics/Menu/bento_meal/Rainbow_Bento/Fushion_text.m
+include ./topics/Menu/bento_meal/Rainbow_Bento/Saveur_text.m
+
+include ./topics/Menu/mini_feast/Appetit_text.m
+include ./topics/Menu/mini_feast/Valueur_text.m
+include ./topics/Menu/mini_feast/Merveille_text.m
+include ./topics/Menu/mini_feast/Superieur_text.m
+include ./topics/Menu/mini_feast/Joyeux_text.m
+
+include ./topics/Menu/refreshment/Rainbow_Snack_Box/Bouchees_text.m
+include ./topics/Menu/refreshment/Rainbow_Snack_Box/Patisserie_text.m
+include ./topics/Menu/refreshment/Rainbow_Snack_Box/Fraiche_text.m
+include ./topics/Menu/refreshment/Rainbow_Snack_Box/Bouffee_text.m
+include ./topics/Menu/refreshment/Rainbow_Snack_Box/Emballee_text.m
+
+include ./topics/Menu/refreshment/High_Tea_Express/Valeur_text.m
+include ./topics/Menu/refreshment/High_Tea_Express/Merveille_text.m
+include ./topics/Menu/refreshment/High_Tea_Express/Superieur_text.m
+
+include ./topics/Menu/wedding/Le_Amour_Mini_Wedding/Matrimony_text.m
+include ./topics/Menu/wedding/Le_Amour_Mini_Wedding/Nuptial_text.m
+
+include ./topics/Menu/wedding/Wedding_Reception/Romance_text.m
+include ./topics/Menu/wedding/Wedding_Reception/Destino_text.m
+include ./topics/Menu/wedding/Wedding_Reception/Eternite_text.m
+include ./topics/Menu/wedding/Wedding_Reception/Charme_text.m
+include ./topics/Menu/wedding/Wedding_Reception/Beaute_text.m
+include ./topics/Menu/wedding/Wedding_Reception/Amore_text.m
