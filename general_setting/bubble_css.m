731f24c7eaf37a7b53410649a487df9850c8011d
C190194 <73808832+C190194@users.noreply.github.com>
2021-07-19 19:42:09 +0800

    Create bubble_css.m

---
+++ general_setting/bubble_css.m
@@ -0,0 +1,24 @@
+\
+\ Copyright © 2021 huangrt all rights reserved.
+\
+\ @author: huangrt
+\ @date: 7 Jul 2021
+\
+\ A brief description of this program.
+\
+
+: bb ( -- ) 
+  q{
+     </div>
+     <div style="padding:10px;border-radius:16px;margin-left: 80px;
+                 text-align:left;vertical-align:middle;
+                 border:solid #d9c1ff 2px;color:#333;
+                 font-family:Open Sans, sans-serif;font-size:15px;
+                 background:#f1f1f1;word-wrap:break-word;
+                 min-width:40px;max-width:280px;">
+   }q
+;
+
+\ Q: $_
+\ A: First bubble ${ bb } second bubble 
+\ --
