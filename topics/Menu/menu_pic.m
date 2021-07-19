854372e173134cc77a5a41f7a26eec81480977e1
C190194 <73808832+C190194@users.noreply.github.com>
2021-07-19 20:20:53 +0800

    Create menu_pic.m

---
+++ topics/Menu/menu_pic.m
@@ -0,0 +1,171 @@
+\
+\ Copyright © 2021 huangrt all rights reserved.
+\
+\ @author: huangrt
+\ @date: 10 Jun 2021
+\
+\ A brief description of this program.
+\
+
+: bento_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153057.898.jpeg"
+;
+
+\ ---- 
+: rainbow_bento_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153057.898.jpeg"
+;
+
+: Bitez_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-35-450x388.jpeg"
+;
+
+: Fushion_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-36-450x388.jpeg"
+;
+
+: Delicieux_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-37-450x388.jpeg"
+;
+
+: Saveur_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-38-450x388.jpeg"
+;
+
+\ ----
+: exquisite_bento_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153404.570.jpeg"
+;
+
+: Mieux_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-39-450x388.jpeg"
+;
+
+: Grande_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-40-450x388.jpeg"
+;
+
+: Gusto_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-41-450x388.jpeg"
+;
+
+: Boite_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-42-450x388.jpeg"
+;
+
+\ ----
+: buffet_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T140513.400.jpeg"
+;
+
+: Appetit_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-27-450x388.jpeg"
+;
+
+: Valueur_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-28-450x388.jpeg"
+;
+
+: Merveille_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-29-450x388.jpeg"
+;
+
+: Superieur_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-30-450x388.jpeg"
+;
+
+: Joyeux_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-31-450x388.jpeg"
+;
+
+\ ----
+: refreshment_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/DSC_3380-1-300x200.jpg"
+;
+
+\ ----
+: rainbow_snack_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153408.390.jpeg"
+;
+
+: Bouchees_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-46-450x388.jpeg"
+;
+
+: Patisserie_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-47-450x388.jpeg"
+;
+
+: Fraiche_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-48-450x388.jpeg"
+;
+
+: Bouffee_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-49-450x388.jpeg"
+;
+
+: Emballee_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-50-450x388.jpeg"
+;
+
+\ ----
+: high_tea_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153406.000.jpeg"
+;
+
+: Valeur_tea_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-43-450x388.jpeg"
+;
+
+: Merveille_tea_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-44-450x388.jpeg"
+;
+
+: Superieur_tea_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-45-450x388.jpeg"
+;
+
+
+\ ----
+: wedding_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2021/04/Wedding-3-450x450.jpg"
+;
+
+\ ----
+: mini_wedding_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T152722.341.jpeg"
+;
+
+: Matrimony_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-21-450x388.jpeg"
+;
+
+: Nuptial_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-22-450x388.jpeg"
+;
+
+\ ----
+: big_wedding_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-2020-10-23T153626.608.jpeg"
+;
+
+: Romance_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-51-450x388.jpeg"
+;
+: Destino_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-52-450x388.jpeg"
+;
+: Eternite_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-53-450x388.jpeg"
+;
+: Charme_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-54-450x388.jpeg"
+;
+: Beaute_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-55-450x388.jpeg"
+;
+: Amore_pic_url
+  "https://lerainbow.com.sg/wp-content/uploads/2020/10/image_mod-56-450x388.jpeg"
+;
+
+
